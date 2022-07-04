3c462f52ab168e19f91051171c6731d2c941ad3c
maraval <maraval@abes.fr>
2022-07-04 09:40:35 +0200

    ODS-115 : Test Ajout contenu notice Test getByPpn : prise en compte de tous les cas possibles

--- core/src/test/java/fr/abes/qualimarc/core/service/NoticeBibioServiceTest.java
+++ core/src/test/java/fr/abes/qualimarc/core/service/NoticeBibioServiceTest.java
@@ -3,10 +3,12 @@ package fr.abes.qualimarc.core.service;
 import fr.abes.qualimarc.core.CoreTestConfiguration;
 import fr.abes.qualimarc.core.configuration.BaseXMLConfiguration;
 import fr.abes.qualimarc.core.entity.notice.NoticesBibio;
+import fr.abes.qualimarc.core.exception.IllegalPpnException;
 import fr.abes.qualimarc.core.repository.basexml.NoticesBibioRepository;
 import org.apache.commons.io.IOUtils;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,6 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.Resource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.sql.rowset.serial.SerialClob;
 import java.io.FileInputStream;
@@ -22,9 +27,9 @@ import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
 import java.util.Optional;
 
-@SpringBootTest(classes = {CoreTestConfiguration.class})
-@ComponentScan(excludeFilters = @ComponentScan.Filter(BaseXMLConfiguration.class))
-public class NoticeBibioServiceTest {
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(classes = {NoticeBibioService.class})
+class NoticeBibioServiceTest {
     @Autowired
     private NoticeBibioService service;
 
@@ -42,8 +47,13 @@ public class NoticeBibioServiceTest {
         notice.setPpn("143519379");
         notice.setDataXml(new SerialClob(xml.toCharArray()));
 
-        Mockito.when(repository.getByPpn("143519379")).thenReturn(Optional.of(notice) );
+        Mockito.when(repository.getByPpn("143519379")).thenReturn(Optional.of(notice));
 
         Assertions.assertThat(service.getByPpn("143519379").getLeader()).isEqualTo("     cam0 22        450 ");
+
+        Mockito.when(repository.getByPpn("111111111")).thenThrow(new IllegalPpnException("le PPN 111111111 n'existe pas"));
+        assertThrows(IllegalPpnException.class, () -> service.getByPpn("111111111"));
+
+        assertThrows(IllegalPpnException.class, () -> service.getByPpn(null));
     }
 }
