5b09a6954b0f56979f607b459c0b6270e3691761
Pierre Maraval <maraval@abes.fr>
2022-07-05 13:50:23 +0200

    ODS-116 : Refactor
    Repackaging en .jar
    Ajout mainClass dans pom

--- web/src/main/java/fr/abes/qualimarc/QualimarcAPIApplication.java
+++ web/src/main/java/fr/abes/qualimarc/QualimarcAPIApplication.java
@@ -3,18 +3,13 @@ package fr.abes.qualimarc;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class QualimarcAPIApplication extends SpringBootServletInitializer implements CommandLineRunner {
+public class QualimarcAPIApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(QualimarcAPIApplication.class, args);
     }
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(QualimarcAPIApplication.class);
-    }
+
     @Override
     public void run(String... args) throws Exception {
     }
