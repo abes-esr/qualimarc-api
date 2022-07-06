2e14cd2de0d21b27de77be983baa69349d4c11ff
EKL <eryne.kachetel-langue@abes.fr>
2022-07-06 11:27:16 +0200

    ODS-95 : feat Ajout de la classe PresenceµSousZone

---
+++ core/src/main/java/fr/abes/qualimarc/core/model/entity/rules/structure/PresenceSousZone.java
@@ -0,0 +1,43 @@
+package fr.abes.qualimarc.core.model.entity.rules.structure;
+
+import fr.abes.qualimarc.core.model.entity.notice.NoticeXml;
+import fr.abes.qualimarc.core.model.entity.rules.Rule;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PresenceSousZone extends Rule {
+
+    private String sousZone;
+    private boolean isPresent;
+
+    public PresenceSousZone(Integer id, String message, String zone, String sousZone, boolean isPresent) {
+        super(id, message, zone);
+        this.sousZone = sousZone;
+        this.isPresent = isPresent;
+    }
+
+    public PresenceSousZone(Integer id, String message, String zone, String sousZone, boolean isPresent, List<String> typeDocuments) {
+        super(id, message, zone, typeDocuments);
+        this.sousZone = sousZone;
+        this.isPresent = isPresent;
+    }
+
+    @Override
+    public boolean isValid(NoticeXml notice) {
+        if(this.isPresent) {
+            return notice.getDatafields().stream().anyMatch(dataField -> {
+                if(dataField.getTag().equals(this.getZone())){
+                    return dataField.getSubFields().stream().anyMatch(subField -> subField.getCode().equals(this.getSousZone()));
+                }
+                return dataField.getSubFields().stream().noneMatch(subField -> subField.getCode().equals(this.getSousZone()));
+            });
+        }
+        return false;
+    }
+}
