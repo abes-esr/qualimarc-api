fabd364979623b0f11e41b289670a9084821f281
EKL <eryne.kachetel-langue@abes.fr>
2022-07-22 09:58:32 +0200

    ODS-102 : feat Modification de la classe RuleSet Modification de la classe RuleSetTest

--- core/src/test/java/fr/abes/qualimarc/core/model/entity/rules/RuleSetTest.java
+++ core/src/test/java/fr/abes/qualimarc/core/model/entity/rules/RuleSetTest.java
@@ -1,8 +1,6 @@
 package fr.abes.qualimarc.core.model.entity.rules;
 
 import fr.abes.qualimarc.core.configuration.BaseXMLConfiguration;
-import fr.abes.qualimarc.core.model.entity.rules.rulesSet.RulesSetType;
-import fr.abes.qualimarc.core.model.entity.rules.structure.PresenceZone;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,44 +15,27 @@ class RuleSetTest {
 
     @Test
     void getResultQuickRulesSet() {
-        PresenceZone rule = new PresenceZone(1, "La zone 010 doit être présente", "010", true);
-        List<Rule> ruleListControl = new ArrayList<>();
-        ruleListControl.add(rule);
-
         //  Test du jeu de règles rapide
-        List<String> focusedRulesSet1 = new ArrayList<>();
-        RulesSetType rulesSetType1 = new RulesSetType(true, false, focusedRulesSet1);
         RuleSet ruleSet1 = new RuleSet();
-        List<Rule> ruleList = ruleSet1.getRuleList(rulesSetType1);
-        Assertions.assertEquals(ruleListControl.get(0).getZone(), ruleList.get(0).getZone());
+        List<Rule> ruleList = ruleSet1.getQuickRulesList();
+        Assertions.assertFalse(ruleList.isEmpty());
     }
 
     @Test
     void getResultCompleteRuleTest() {
-        PresenceZone rule = new PresenceZone(1, "La zone 020 doit être présente", "020", true);
-        List<Rule> ruleListControl = new ArrayList<>();
-        ruleListControl.add(rule);
-
         //  Test du jeu de règles complet
-        List<String> focusedRulesSet2 = new ArrayList<>();
-        RulesSetType rulesSetType2 = new RulesSetType(false, true, focusedRulesSet2);
         RuleSet ruleSet2 = new RuleSet();
-        List<Rule> ruleList = ruleSet2.getRuleList(rulesSetType2);
-        Assertions.assertEquals(ruleListControl.get(0).getZone(), ruleList.get(1).getZone());
+        List<Rule> ruleList = ruleSet2.getAdvancedRulesList();
+        Assertions.assertFalse(ruleList.isEmpty());
     }
 
     @Test
     void getResultFocusedRuleTest() {
-        PresenceZone rule = new PresenceZone(1, "La zone 030 doit être présente", "030", true);
-        List<Rule> ruleListControl = new ArrayList<>();
-        ruleListControl.add(rule);
-
         //  Test du jeu de règles complet
-        List<String> focusedRulesSet3 = new ArrayList<>();
-        focusedRulesSet3.add("03");
-        RulesSetType rulesSetType3 = new RulesSetType(false, false, focusedRulesSet3);
         RuleSet ruleSet3 = new RuleSet();
-        List<Rule> ruleList = ruleSet3.getRuleList(rulesSetType3);
-        Assertions.assertEquals(ruleListControl.get(0).getZone(), ruleList.get(0).getZone());
+        List<String> focusedSetList = new ArrayList<>();
+        focusedSetList.add("P4");
+        List<Rule> ruleList = ruleSet3.getFocusedRulesList(focusedSetList);
+        Assertions.assertFalse(ruleList.isEmpty());
     }
 }
