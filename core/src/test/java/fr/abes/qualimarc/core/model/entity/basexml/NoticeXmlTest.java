74420ac13eff468ccf02018eeb947a1a116afdb1
pierre-maraval <pierre.maraval@gmail.com>
2022-10-04 11:04:20 +0200

    ODS-157 : refactor : optimisation récupération date de modification notice

--- core/src/test/java/fr/abes/qualimarc/core/model/entity/basexml/NoticeXmlTest.java
+++ core/src/test/java/fr/abes/qualimarc/core/model/entity/basexml/NoticeXmlTest.java
@@ -1,7 +1,6 @@
 package fr.abes.qualimarc.core.model.entity.basexml;
 
 import fr.abes.qualimarc.core.exception.noticexml.AuteurNotFoundException;
-import fr.abes.qualimarc.core.exception.noticexml.IsbnNotFoundException;
 import fr.abes.qualimarc.core.exception.noticexml.TitreNotFoundException;
 import fr.abes.qualimarc.core.exception.noticexml.ZoneNotFoundException;
 import fr.abes.qualimarc.core.model.entity.notice.Controlfield;
@@ -125,7 +124,7 @@ class NoticeXmlTest {
         datafields.add(datafield);
         notice.setDatafields(datafields);
 
-        Assertions.assertEquals("Auteur Test", notice.getAuteur());
+        Assertions.assertEquals("Auteur test", notice.getAuteur());
     }
 
     @Test
@@ -165,23 +164,7 @@ class NoticeXmlTest {
         datafields.add(datafield);
         notice.setDatafields(datafields);
 
-
-        NoticeXml notice2 = new NoticeXml();
-        notice2.setLeader("");
-        List<Datafield> datafields2 = new ArrayList<>();
-        Datafield datafield2 = new Datafield();
-        datafield2.setTag("010");
-        List<SubField> subFields2 = new ArrayList<>();
-        SubField subField2 = new SubField();
-        subField2.setCode("A");
-        subField2.setValue("9781334567890");
-        subFields2.add(subField2);
-        datafield2.setSubFields(subFields2);
-        datafields2.add(datafield2);
-        notice2.setDatafields(datafields2);
-
         Assertions.assertEquals("1234567890", notice.getIsbn());
-        Assertions.assertEquals("9781334567890", notice2.getIsbn());
     }
 
     @Test
@@ -269,30 +252,4 @@ class NoticeXmlTest {
         Assertions.assertEquals("07/04/2022", notice.getDateModification());
     }
 
-    @Test
-    void getDateModificationWith004And005() throws ParseException {
-        NoticeXml notice = new NoticeXml();
-        Controlfield controlfield004 = new Controlfield();
-        controlfield004.setTag("004");
-        controlfield004.setValue("19901122");
-
-        Controlfield controlfield005 = new Controlfield();
-        controlfield005.setTag("005");
-        controlfield005.setValue("20220407143046.000");
-        notice.setControlfields(Lists.newArrayList(controlfield004, controlfield005));
-
-        Assertions.assertEquals("07/04/2022", notice.getDateModification());
-    }
-
-    @Test
-    void getDateModificationWith004AndWithout005() throws ParseException {
-        NoticeXml notice = new NoticeXml();
-        Controlfield controlfield004 = new Controlfield();
-        controlfield004.setTag("004");
-        controlfield004.setValue("19901122");
-
-        notice.setControlfields(Lists.newArrayList(controlfield004));
-
-        Assertions.assertEquals("22/11/1990", notice.getDateModification());
-    }
 }
