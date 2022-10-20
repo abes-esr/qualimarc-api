baec5958d6d4582110aa5f18e2c0ef358e59bd12
EryneKL <97091460+EryneKL@users.noreply.github.com>
2022-10-20 10:28:42 +0200

    ODS-242 : feat    - refactor de la règle PresenceChaineCaracteres    - début de l'écriture des TU

--- core/src/test/java/fr/abes/qualimarc/core/model/entity/qualimarc/rules/structure/PresenceChaineCaracteresTest.java
+++ core/src/test/java/fr/abes/qualimarc/core/model/entity/qualimarc/rules/structure/PresenceChaineCaracteresTest.java
@@ -9,6 +9,7 @@ import fr.abes.qualimarc.core.utils.BooleanOperateur;
 import fr.abes.qualimarc.core.utils.EnumChaineCaracteres;
 import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
@@ -29,15 +30,19 @@ class PresenceChaineCaracteresTest {
     @Value("classpath:143519379.xml")
     private Resource xmlFileNotice;
 
-    @Test
-    void isValid() throws IOException {
+    private NoticeXml notice;
+
+    @BeforeEach
+    void init() throws IOException {
         String xml = IOUtils.toString(new FileInputStream(xmlFileNotice.getFile()), StandardCharsets.UTF_8);
         JacksonXmlModule module = new JacksonXmlModule();
         module.setDefaultUseWrapper(false);
         XmlMapper mapper = new XmlMapper(module);
-        NoticeXml notice = mapper.readValue(xml, NoticeXml.class);
+        this.notice = mapper.readValue(xml, NoticeXml.class);
+    }
 
-
+    @Test
+    void isValid() throws IOException {
         // Teste la présence de la zone
         ChaineCaracteres chaineCaracteres = new ChaineCaracteres(BooleanOperateur.ET, EnumChaineCaracteres.STRICTEMENT, "");
         List<ChaineCaracteres> listChaineCaracteres = new ArrayList<>();
@@ -45,15 +50,6 @@ class PresenceChaineCaracteresTest {
         PresenceChaineCaracteres presenceChaineCaracteres = new PresenceChaineCaracteres(0, "999", "", listChaineCaracteres);
 
         Assertions.assertFalse(presenceChaineCaracteres.isValid(notice));
-
-
-        // Teste la présence de la sous-zone
-        ChaineCaracteres chaineCaracteres0 = new ChaineCaracteres(BooleanOperateur.ET, EnumChaineCaracteres.STRICTEMENT, "");
-        List<ChaineCaracteres> listChaineCaracteres0 = new ArrayList<>();
-        listChaineCaracteres0.add(chaineCaracteres0);
-        PresenceChaineCaracteres presenceChaineCaracteres0 = new PresenceChaineCaracteres(0, "200", "g", listChaineCaracteres0);
-
-        Assertions.assertFalse(presenceChaineCaracteres0.isValid(notice));
 
 
         //  Teste si une occurence de la sous-zone contient STRICTEMENT la chaine de caractères
