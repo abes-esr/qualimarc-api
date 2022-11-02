43825da0b7bfb1a5b61c48a5b342216a2425408f
SamuelQuetin <samquetinuel@gmail.com>
2022-11-02 08:31:15 +0100

    ODS 221: FIX webdto

--- web/src/main/java/fr/abes/qualimarc/web/dto/reference/RuleSetWebDto.java
+++ web/src/main/java/fr/abes/qualimarc/web/dto/reference/RuleSetWebDto.java
@@ -1,7 +1,13 @@
 package fr.abes.qualimarc.web.dto.reference;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class RuleSetWebDto {
     @JsonProperty("id")
     private Integer id;
