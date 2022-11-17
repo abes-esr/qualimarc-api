5e50e27b806e6121a274f1d6173dbbec6436ee52
quetin <quetin@abes.fr>
2022-11-17 15:43:02 +0100

    Fix isvald

--- core/src/main/java/fr/abes/qualimarc/core/model/entity/qualimarc/rules/contenu/ComparaisonDate.java
+++ core/src/main/java/fr/abes/qualimarc/core/model/entity/qualimarc/rules/contenu/ComparaisonDate.java
@@ -12,8 +12,6 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.io.Serializable;
-import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Entity
@@ -42,51 +40,41 @@ public class ComparaisonDate extends SimpleRule implements Serializable {
     @Override
     public boolean isValid(NoticeXml... notices) {
         NoticeXml notice = notices[0];
-        List<Datafield> zonesSource = notice.getDatafields().stream().filter(d -> d.getTag().equals(this.getZone())).collect(Collectors.toList());
-        List<Datafield> zonesSourceCible = notice.getDatafields().stream().filter(d -> d.getTag().equals(this.getZoneCible())).collect(Collectors.toList());
-        boolean isOk = false;
-        for (Datafield zoneSource : zonesSource){
-            for (Datafield zoneSourceCible : zonesSourceCible){
-                for (SubField subField : zoneSource.getSubFields().stream().filter(ss -> ss.getCode().equals(this.getSousZone())).collect(Collectors.toList())) {
-                    for (SubField subFieldCible : zoneSourceCible.getSubFields().stream().filter(ss -> ss.getCode().equals(this.getSousZoneCible())).collect(Collectors.toList())) {
-                        //Controle de la zone pour verifier le contenu (date avec X ? date avec 4 chiffres ?)
-                        String dateSourceString = subField.getValue();
-                        String dateCibleString = subFieldCible.getValue();
+        Datafield datafieldSource = notice.getDatafields().stream()
+                .filter(datafield -> datafield.getTag().equals(this.getZone()) && datafield.getSubFields().stream().anyMatch(subField -> subField.getCode().equals(this.getSousZone())))
+                .findFirst().orElse(null);
+        Datafield datafieldCible = notice.getDatafields().stream()
+                .filter(datafield -> datafield.getTag().equals(this.getZoneCible()) && datafield.getSubFields().stream().anyMatch(subField -> subField.getCode().equals(this.getSousZoneCible())))
+                .findFirst().orElse(null);
 
-                        if(this.getPositionStart() != null && this.getPositionEnd() != null)
-                             dateSourceString = dateSourceString.substring(this.getPositionStart(), this.getPositionEnd());
-
-                        if(this.getPositionStartCible() != null && this.getPositionEndCible() != null)
-                            dateCibleString = dateCibleString.substring(this.getPositionStartCible(), this.getPositionEndCible());
-
-                        if(dateSourceString.toUpperCase().contains("X") || dateCibleString.toUpperCase().contains("X") || dateSourceString.length() != 4 || dateCibleString.length() != 4)
+        if (datafieldSource != null && datafieldCible != null) {
+            SubField subFieldSource = datafieldSource.getSubFields().stream().filter(subField -> subField.getCode().equals(this.getSousZone())).findFirst().orElse(null);
+            SubField subFieldCible = datafieldCible.getSubFields().stream().filter(subField -> subField.getCode().equals(this.getSousZoneCible())).findFirst().orElse(null);
+            if (subFieldSource != null && subFieldCible != null) {
+                String valueSource = subFieldSource.getValue();
+                String valueCible = subFieldCible.getValue();
+                if (this.getPositionEnd() != null && valueSource.length() >= this.getPositionEnd()) {
+                    valueSource = valueSource.substring(this.getPositionStart(), this.getPositionEnd());
+                }
+                if(this.getPositionEndCible() != null && valueCible.length() >= this.getPositionEndCible()){
+                    valueCible = valueCible.substring(this.getPositionStartCible(), this.getPositionEndCible());
+                }
+                if(valueSource.length() == 4 && valueCible.length() == 4 && !(valueSource.contains("X") || valueCible.contains("X"))) {
+                    switch (this.getOperateur()) {
+                        case EGAL:
+                            return valueSource.equals(valueCible);
+                        case INFERIEUR:
+                            return valueSource.compareTo(valueCible) < 0;
+                        case SUPERIEUR:
+                            return valueSource.compareTo(valueCible) > 0;
+                        case INFERIEUR_EGAL:
+                            return valueSource.compareTo(valueCible) <= 0;
+                        case SUPERIEUR_EGAL:
+                            return valueSource.compareTo(valueCible) >= 0;
+                        default:
                             return false;
-
-                        Integer dateSource = Integer.parseInt(dateSourceString);
-                        Integer dateCible = Integer.parseInt(dateCibleString);
-                        switch (this.getOperateur()) {
-                            case SUPERIEUR:
-                                isOk = dateSource > dateCible;
-                                break;
-                            case INFERIEUR:
-                                isOk = dateSource < dateCible;
-                                break;
-                            case EGAL:
-                                isOk = dateSource.equals(dateCible);
-                                break;
-                            case SUPERIEUR_EGAL:
-                                isOk = dateSource >= dateCible;
-                                break;
-                            case INFERIEUR_EGAL:
-                                isOk = dateSource <= dateCible;
-                                break;
-                        }
-                        if(isOk){
-                            return true;
-                        }
                     }
                 }
-
             }
         }
         return false;
