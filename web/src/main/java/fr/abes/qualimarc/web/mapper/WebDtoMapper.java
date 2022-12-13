b863301c6273ee40fa09b9b5a27b91c447ffe1af
quetin <quetin@abes.fr>
2022-12-13 16:20:39 +0100

    Ajustement de la webdto, a finir le mapper

--- web/src/main/java/fr/abes/qualimarc/web/mapper/WebDtoMapper.java
+++ web/src/main/java/fr/abes/qualimarc/web/mapper/WebDtoMapper.java
@@ -225,12 +225,8 @@ public class WebDtoMapper {
             @Override
             public ComplexRule convert(MappingContext<ComparaisonContenuSousZoneWebDto, ComplexRule> context) {
                 ComparaisonContenuSousZoneWebDto source = context.getSource();
-                Integer nombreCaracteres = null;
-                if (source.getNombreCaracteres() != null) {
-                    nombreCaracteres = convertNombreCaracteres(source.getNombreCaracteres());
-                }
                 checkOtherRule(source);
-                return new ComplexRule(source.getId(), source.getMessage(), getPriority(source.getPriority()), getFamilleDocument(source.getTypesDoc()), getTypeThese(source.getTypesThese()), getRuleSet(source.getRuleSetList()), new ComparaisonContenuSousZone(source.getId(), source.getZone(), source.getSousZone(), getTypeDeVerification(source.getTypeVerification()), nombreCaracteres, source.getZoneCible(), source.getSousZoneCible()));
+                return new ComplexRule(source.getId(), source.getMessage(), getPriority(source.getPriority()), getFamilleDocument(source.getTypesDoc()), getTypeThese(source.getTypesThese()), getRuleSet(source.getRuleSetList()), new ComparaisonContenuSousZone(source.getId(), source.getZone(), source.getSousZone(), getTypeDeVerification(source.getTypeVerification()), convertNombreCaracteres(source.getNombreCaracteres()), source.getZoneCible(), source.getSousZoneCible()));
             }
         };
         mapper.addConverter(myConverter);
@@ -355,11 +351,7 @@ public class WebDtoMapper {
         Converter<ComparaisonContenuSousZoneWebDto, SimpleRule> myConverter = new Converter<ComparaisonContenuSousZoneWebDto, SimpleRule>() {
             public SimpleRule convert(MappingContext<ComparaisonContenuSousZoneWebDto, SimpleRule> context) {
                 ComparaisonContenuSousZoneWebDto source = context.getSource();
-                Integer nombreCaracteres = null;
-                if (source.getNombreCaracteres() != null) {
-                    nombreCaracteres = convertNombreCaracteres(source.getNombreCaracteres());
-                }
-                return new ComparaisonContenuSousZone(source.getId(), source.getZone(), source.getSousZone(), getTypeDeVerification(source.getTypeVerification()), nombreCaracteres, source.getZoneCible(), source.getSousZoneCible());
+                return new ComparaisonContenuSousZone(source.getId(), source.getZone(), source.getSousZone(), getTypeDeVerification(source.getTypeVerification()), convertNombreCaracteres(source.getNombreCaracteres()), source.getZoneCible(), source.getSousZoneCible());
             }
         };
         mapper.addConverter(myConverter);
@@ -431,7 +423,7 @@ public class WebDtoMapper {
                 ComplexRuleWebDto source = context.getSource();
                 ComplexRule target;
                 //vérification qu'aucune règle simple ne contient de zone générique
-                if (source.getRegles().stream().anyMatch(rule -> rule.getZone().matches("\\dXX"))) {
+                if (source.getRegles().stream().anyMatch(rule -> (rule.getZone() != null && rule.getZone().matches("\\dXX")))) {
                     throw new IllegalArgumentException("Une règle complexe ne peut pas contenir de règles simple avec des zones génériques");
                 }
                 Iterator<SimpleRuleWebDto> reglesIt = source.getRegles().listIterator();
@@ -442,7 +434,17 @@ public class WebDtoMapper {
                     throw new IllegalArgumentException("La première règle d'une règle complexe ne peut pas être une règle de réciprocité");
                 int i = 0;
                 if (null == firstRegle.getBooleanOperator()) {
-                    target = new ComplexRule(source.getId(), source.getMessage(), getPriority(source.getPriority()), mapper.map(firstRegle, SimpleRule.class));
+                    if(source.getZone() != null){
+                        //verifier que la regle soit du bon type : [ presenceZone, presenceSousZone, presenceChaineCaracteres, indicateur, positionSousZone ]
+                        checkRuleWebDtoIsAInstanseMemeZoneRules(firstRegle);
+                        firstRegle.setZone(source.getZone());
+                        SimpleRule firstRegleEntity = mapper.map(firstRegle, SimpleRule.class);
+                        target = new ComplexRule(source.getId(), source.getMessage(), getPriority(source.getPriority()), firstRegleEntity);
+                        target.setMemeZone(true);
+                        firstRegleEntity.setComplexRule(target);
+                    }else{
+                        target = new ComplexRule(source.getId(), source.getMessage(), getPriority(source.getPriority()), mapper.map(firstRegle, SimpleRule.class));
+                    }
                     if (source.getRuleSetList() != null) {
                         target.setRuleSet(getRuleSet(source.getRuleSetList()));
                     }
@@ -474,8 +476,16 @@ public class WebDtoMapper {
                             target.addOtherRule(new DependencyRule(otherRegle.getId(), otherRegle.getZone(), ((DependencyWebDto) otherRegle).getSousZone(), getTypeNoticeLiee(((DependencyWebDto) otherRegle).getTypeNoticeLiee()), i++, target));
                             isDependencyRuleCreated = true;
                         }
-                        else
-                            target.addOtherRule(new LinkedRule(mapper.map(otherRegle, SimpleRule.class), isPreviousRegleDependency ? BooleanOperateur.ET : getOperateur(otherRegle.getBooleanOperator()), i++, target));
+                        else {
+                            if(source.getZone() != null) {
+                                //verifier que la regle soit du bon type : [ presenceZone, presenceSousZone, presenceChaineCaracteres, indicateur, positionSousZone ]
+                                checkRuleWebDtoIsAInstanseMemeZoneRules(otherRegle);
+                                otherRegle.setZone(source.getZone());
+                                target.addOtherRule(new LinkedRule(mapper.map(otherRegle, SimpleRule.class),  BooleanOperateur.ET, i++, target));
+                            } else {
+                                target.addOtherRule(new LinkedRule(mapper.map(otherRegle, SimpleRule.class), isPreviousRegleDependency ? BooleanOperateur.ET : getOperateur(otherRegle.getBooleanOperator()), i++, target));
+                            }
+                        }
                         isPreviousRegleDependency = otherRegle instanceof DependencyWebDto;
                     }
 
@@ -735,6 +745,9 @@ public class WebDtoMapper {
         if (source.getMessage() == null || source.getPriority() == null) {
             throw new IllegalArgumentException("Règle " + source.getId() + " : Le message et / ou la priorité est obligatoire lors de la création d'une règle simple");
         }
+        if (source.getZone() == null) {
+            throw new IllegalArgumentException("Règle " + source.getId() + " : La zone est obligatoire lors de la création d'une règle simple");
+        }
         checkTypeThese(source.getTypesThese());
     }
 
@@ -753,6 +766,14 @@ public class WebDtoMapper {
         }
     }
 
+    private void checkRuleWebDtoIsAInstanseMemeZoneRules(SimpleRuleWebDto rule){
+        if(!(rule instanceof PresenceZoneWebDto) && !(rule instanceof PresenceSousZoneWebDto) && !(rule instanceof PositionSousZoneWebDto) && !(rule instanceof PresenceChaineCaracteresWebDto) && !(rule instanceof IndicateurWebDto)){
+            throw new IllegalArgumentException("Règle " + rule.getId() + " : La règle n'est pas une règle qui peut s'appliquer sur une même instance de zone");
+        }
+        if(rule.getZone() != null){
+            throw new IllegalArgumentException("Les règles complexe ne peuvent pas contenir de règles simples avec des zones différentes");
+        }
+    }
     private TypeCaractere constructTypeCaractere(TypeCaractereWebDto source) {
         if(source.getTypeCaracteres().isEmpty()){
             throw new IllegalArgumentException("Règle " + source.getId() + " : Le champ type-caracteres est obligatoire");
@@ -765,6 +786,8 @@ public class WebDtoMapper {
     }
 
     private Integer convertNombreCaracteres(String nombreCarateres) {
+        if(nombreCarateres == null)
+            return null;
         return Integer.valueOf(nombreCarateres);
     }
 }
