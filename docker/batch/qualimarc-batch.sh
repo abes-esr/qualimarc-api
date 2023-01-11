2f1c259d64ce251e607a96076f4f1862253a795d
SamuelQuetin <samquetinuel@gmail.com>
2023-01-11 10:25:34 +0100

    fix sh

--- docker/batch/qualimarc-batch.sh
+++ docker/batch/qualimarc-batch.sh
@@ -1,4 +1,4 @@
 if [[ $(pgrep -cf "qualimarc-batch.jar --spring.batch.job.names=exportStatistiques") = 0 ]];
 then
-    java -jar qualimarc-batch.jar --spring.batch.job.names=exportStatistiques
+    java -jar /scripts/qualimarc-batch.jar --spring.batch.job.names=exportStatistiques
 fi
