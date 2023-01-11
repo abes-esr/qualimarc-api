231458e1b3a23276c1df3aca0a29656044272217
EryneKL <97091460+EryneKL@users.noreply.github.com>
2023-01-11 14:57:31 +0100

    FEAT : ODS-517     - ajout d'un paramètre TypeVerification dans Indicateur.java     - modification de IndicateurTest.java     - ajout d'un paramtètre String typeDeVerification dans IndicateurWebDto.java     - modification de WebDtoMapper.java, WebDtoMapperTest.java, ComplexRuleTest.java et RuleController.java

--- web/src/main/java/fr/abes/qualimarc/web/dto/indexrules/contenu/IndicateurWebDto.java
+++ web/src/main/java/fr/abes/qualimarc/web/dto/indexrules/contenu/IndicateurWebDto.java
@@ -24,16 +24,23 @@ public class IndicateurWebDto extends SimpleRuleWebDto {
     @JsonProperty("valeur")
     private String valeur;
 
-    public IndicateurWebDto(Integer id, Integer idExcel, List<Integer> ruleSetList, String message, String zone, String priority, List<String> typesDoc, List<String> typesThese, Integer indicateur, String valeur) {
+    @Pattern(regexp = "STRICTEMENT|STRICTEMENTDIFFERENT", message = "Le champ niveau de verification ne peut contenir que STRICTEMENT ou STRICTEMENTDIFFERENT")
+    @JsonProperty("type-de-verification")
+    @NotNull
+    private String typeDeVerification;
+
+    public IndicateurWebDto(Integer id, Integer idExcel, List<Integer> ruleSetList, String message, String zone, String priority, List<String> typesDoc, List<String> typesThese, Integer indicateur, String valeur, String typeDeVerification) {
         super(id, idExcel, ruleSetList, message, zone, priority, typesDoc, typesThese);
         this.indicateur = indicateur;
         this.valeur = valeur;
+        this.typeDeVerification = typeDeVerification;
     }
 
-    public IndicateurWebDto(Integer id, String zone, String booleanOperator, Integer indicateur, String valeur) {
+    public IndicateurWebDto(Integer id, String zone, String booleanOperator, Integer indicateur, String valeur, String typeDeVerification) {
         super(id, zone, booleanOperator);
         this.indicateur = indicateur;
         this.valeur = valeur;
+        this.typeDeVerification = typeDeVerification;
     }
 
     public IndicateurWebDto() {
