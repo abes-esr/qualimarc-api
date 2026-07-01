971031e2b37aa99db2014371b033adcb6b63be31
Jerome Villiseck <jvk@abes.fr>
2026-07-01 15:24:24 +0200

    SOA-183 : rendre statusApplication tolerante aux timeouts BaseXML

--- core/src/main/java/fr/abes/qualimarc/core/repository/basexml/NoticesBibioRepository.java
+++ core/src/main/java/fr/abes/qualimarc/core/repository/basexml/NoticesBibioRepository.java
@@ -2,7 +2,9 @@ package fr.abes.qualimarc.core.repository.basexml;
 
 import fr.abes.qualimarc.core.configuration.BaseXMLConfiguration;
 import fr.abes.qualimarc.core.model.entity.basexml.NoticesBibio;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 import java.util.Calendar;
@@ -13,5 +15,6 @@ import java.util.Optional;
 public interface NoticesBibioRepository extends JpaRepository<NoticesBibio, Integer> {
     Optional<NoticesBibio> getByPpn(String ppn);
 
+    @QueryHints(@QueryHint(name = "jakarta.persistence.query.timeout", value = "2000"))
     NoticesBibio findFirstByDateEtatBeforeOrderByDateEtatDesc(Calendar date);
 }
