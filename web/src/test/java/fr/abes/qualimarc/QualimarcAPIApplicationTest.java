433efd9c81ea34218abfc5ce32a42213b1ea7e83
Jerome Villiseck <jvk@abes.fr>
2026-07-03 07:23:46 +0200

    test: renforcer le test de demarrage du token API

--- web/src/test/java/fr/abes/qualimarc/QualimarcAPIApplicationTest.java
+++ web/src/test/java/fr/abes/qualimarc/QualimarcAPIApplicationTest.java
@@ -1,14 +1,31 @@
 package fr.abes.qualimarc;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.CommandLineRunner;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@ExtendWith(OutputCaptureExtension.class)
 class QualimarcAPIApplicationTest {
 
     @Test
-    void applicationDoesNotImplementCommandLineRunnerAnymore() {
-        assertThat(CommandLineRunner.class.isAssignableFrom(QualimarcAPIApplication.class)).isFalse();
+    void startupDoesNotEmitConfiguredApiToken(CapturedOutput output) {
+        String configuredToken = "token-admin-qualimarc";
+        String[] args = {
+                "--jwt.token=" + configuredToken,
+                "--jwt.anonymousUser=qualimarcUser"
+        };
+
+        try (MockedStatic<SpringApplication> springApplication = Mockito.mockStatic(SpringApplication.class)) {
+            QualimarcAPIApplication.main(args);
+
+            assertThat(output).doesNotContain(configuredToken);
+            springApplication.verify(() -> SpringApplication.run(QualimarcAPIApplication.class, args));
+        }
     }
 }
